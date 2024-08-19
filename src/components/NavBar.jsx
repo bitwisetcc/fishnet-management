f29c76b7c56155fedcbb7b13801188fa875faab4
ruangodoi <161519850+ruangodoi@users.noreply.github.com>
2024-08-18 22:19:01 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { FaFishFins } from "react-icons/fa6";
 import {
   HomeIcon,
   DocumentTextIcon,
-  Cog6ToothIcon,
+  Cog8ToothIcon as CogIcon, // Novo ícone
   UserGroupIcon,
   ArrowLeftStartOnRectangleIcon as LogOutIcon,
 } from "@heroicons/react/24/solid";
@@ -14,18 +14,18 @@ import logo from "../Logo.png";
 
 const NavBar = () => {
   return (
-    <nav className="hidden sticky md:flex flex-col justify-between items-center list-none pb-12 p-5 mr-2 h-[100vh] top-0 left-0">
-      <ul className="list-none flex flex-col space-y-5 items-center">
+    <nav className="hidden sticky md:flex flex-col justify-between items-center list-none pb-12 p-5 mr-2 h-[100vh] top-0 left-0 bg-backnav-dark shadow-right-lg">
+      <ul className="list-none flex flex-col space-y-7 items-center">
         <img
           src={logo}
           alt="Logo"
-          className="rounded-full border border-slate-600 shadow-sm p-[6px] size-14"
+          className="rounded-full border border-slate-600 shadow-sm p-[6px] w-14 h-14"
         />
         <NavItem Icon={HomeIcon} text="Home" url="/" />
         <NavItem Icon={FaFishFins} text="Produtos" url="/prods" />
         <NavItem Icon={DocumentTextIcon} text="Vendas" url="/vendas" />
         <NavItem Icon={UserGroupIcon} text="Usuários" url="/users" />
-        <NavItem Icon={Cog6ToothIcon} text="Configurações" url="/config" />
+        <NavItem Icon={CogIcon} text="Configurações" url="/config" /> {/* Atualizado */}
       </ul>
       <NavItem Icon={LogOutIcon} text="Logout" url="/login" />
     </nav>
@@ -35,9 +35,9 @@ const NavBar = () => {
 function NavItem({ Icon, text, url }) {
   return (
     <li className="only-of-type:stroke-[10px]">
-      <Tippy content={text} placement="right" delay={50} arrow>
-        <Link to={url}>
-          <Icon className="size-7 text-sky-950 hover:text-highlighy-dimm transition-colors" />
+      <Tippy content={text} placement="bottom" delay={50} arrow>
+        <Link to={url} className="flex items-center">
+          <Icon className="w-7 h-7 text-white hover:text-yellow-300 transition-colors duration-300" />
         </Link>
       </Tippy>
     </li>
