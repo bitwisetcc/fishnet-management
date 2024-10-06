ed77c4c2009250976e96c3aff4f39e9837e492c6
Jorge Terence <jorge.terence@outlook.com>
2024-10-06 11:45:35 -0300

    connected pfp upload

--- src/Pages/TelaPerfilUser.jsx
+++ src/Pages/TelaPerfilUser.jsx
@@ -1,12 +1,30 @@
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
+import { PaperAirplaneIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Tippy from "@tippyjs/react";
 import React, { useContext, useEffect, useState } from "react";
 import { TitleContext } from "../App";
-import { API_URL } from "../lib/query";
 import { useAuth } from "../lib/auth";
+import { API_URL } from "../lib/query";
 
 function TelaPerfilUser() {
   const setTitle = useContext(TitleContext);
   const [profile, setProfile] = useState({});
+  const [pictureDialogOpen, setPictureDialogOpen] = useState(false);
   useAuth(setProfile);
+
+  function changeProfilePicture(profilePic) {
+    fetch(`${API_URL}/users/me`, {
+      method: "PUT",
+      headers: { Authorization: localStorage.getItem("token") },
+      body: { profilePic },
+    })
+      .then((res) => {
+        profile.picture = profilePic;
+        setProfile(profile);
+        console.log(res);
+      })
+      .catch(console.error);
+  }
 
   useEffect(() => {
     setTitle("Minha Conta");
@@ -21,17 +39,24 @@ function TelaPerfilUser() {
   return (
     <div className="bg-white rounded-lg shadow-lg w-full max-w-3xl p-8 flex flex-col md:flex-row items-center border-2 border-gray-light mx-auto">
       <div className="w-48 h-48 md:w-56 md:h-56 rounded-full overflow-hidden border-4 border-blue-dark">
-        <img
-          src={profile.picture}
-          alt="Foto de perfil"
-          className="w-full h-full object-cover"
-        />
+        <Tippy
+          content="Trocar foto de perfil"
+          placement="bottom"
+          delay={50}
+          arrow
+        >
+          <button onClick={() => setPictureDialogOpen(true)}>
+            <img
+              src={profile.picture}
+              alt="Foto de perfil"
+              className="w-full h-full object-cover"
+            />
+          </button>
+        </Tippy>
       </div>
 
       <div className="md:ml-8 mt-6 md:mt-0 flex-1">
-        <h2 className="text-3xl font-bold text-blue-dark">
-          {profile.name}
-        </h2>
+        <h2 className="text-3xl font-bold text-blue-dark">{profile.name}</h2>
         <p className="text-gray-700 mt-2">
           Vendo peixes ornamentais há mais de 20 anos, tenho bastante
           experiência no mercado.
@@ -61,6 +86,43 @@ function TelaPerfilUser() {
           </ul>
         </div>
       </div>
+
+      <Dialog
+        open={pictureDialogOpen}
+        onClose={() => setPictureDialogOpen(false)}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-stone-900/40">
+          <DialogPanel className="w-3/4 lg:w-2/5 space-y-4 border bg-white p-5 rounded-lg">
+            <div className="flex justify-between">
+              <DialogTitle className="font-bold">
+                Nova foto de perfil
+              </DialogTitle>
+              <button onClick={() => setPictureDialogOpen(false)}>
+                <XMarkIcon className="size-5" />
+              </button>
+            </div>
+
+            <form
+              className="flex gap-2"
+              onSubmit={(e) => {
+                e.preventDefault();
+                changeProfilePicture(e.target.pfp.value);
+              }}
+            >
+              <input
+                type="url"
+                name="pfp"
+                id="pfp"
+                className="border border-stone-300 rounded shadow-sm py-1 px-2 flex-1"
+              />
+              <button type="submit" className="bg-alt rounded shadow p-2">
+                <PaperAirplaneIcon className="size-5 text-stone-200" />
+              </button>
+            </form>
+          </DialogPanel>
+        </div>
+      </Dialog>
     </div>
   );
 }
