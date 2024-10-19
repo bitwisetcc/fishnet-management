ba2fd5c0f46fdbdb53ef810f367e80de09388820
Jorge Terence <jorge.terence@outlook.com>
2024-10-19 13:13:37 -0300

    removed test localhost connection

--- src/lib/query.js
+++ src/lib/query.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://fishnet-api-py.onrender.com";
-export const API_URL = "http://localhost:5000";
+export const API_URL = "https://fishnet-api-py.onrender.com";
+// export const API_URL = "http://localhost:5000";
 
 function parseProduct(prod) {
   return {
